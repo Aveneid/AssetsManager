f3e59c70663e4a27aabf3ad4dd029e709e8f4fed
Aveneid <dj.exers@gmail.com>
2024-04-16 13:56:44 +0200

    find employee relations form and logic

---
+++ Forms/Company/FindRelations/FindEmployeeRelations.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FindEmployeeRelations
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FindEmployeeRelations))
+        Me.wbEmployeeRelations = New System.Windows.Forms.WebBrowser()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'wbEmployeeRelations
+        '
+        Me.wbEmployeeRelations.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.wbEmployeeRelations.Location = New System.Drawing.Point(0, 25)
+        Me.wbEmployeeRelations.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wbEmployeeRelations.Name = "wbEmployeeRelations"
+        Me.wbEmployeeRelations.Size = New System.Drawing.Size(784, 401)
+        Me.wbEmployeeRelations.TabIndex = 2
+        Me.wbEmployeeRelations.Url = New System.Uri("about:blank", System.UriKind.Absolute)
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(784, 25)
+        Me.ToolStrip1.TabIndex = 3
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'FindEmployeeRelations
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 426)
+        Me.Controls.Add(Me.wbEmployeeRelations)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "FindEmployeeRelations"
+        Me.Text = "FindEmployeeRelations"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents wbEmployeeRelations As WebBrowser
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+End Class
---
+++ Forms/Company/FindRelations/FindEmployeeRelations.vb
@@ -0,0 +1,48 @@
+﻿Public Class FindEmployeeRelations
+    Public _emp As Employee
+    Dim dtWarehouses As DataTable
+    Dim dtDepartments As DataTable
+    Dim dtMachines As DataTable
+
+    Private Sub FindEmployeeRelations_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dtWarehouses = Globals.findEmployeeRelations(_emp.id, 0)
+        dtMachines = Globals.findEmployeeRelations(_emp.id, 1)
+
+        prepareTables()
+
+        generatePage(wbEmployeeRelations)
+    End Sub
+
+    Sub prepareTables()
+        Dim tmpWare As DataTable = New DataTable
+        tmpWare.Columns.Add("name")
+        tmpWare.Columns.Add("desc")
+        tmpWare.Columns.Add("idx")
+        tmpWare.Columns.Add("dept")
+        tmpWare.Columns.Add("resp_emp")
+
+
+
+        For Each r As DataRow In dtWarehouses.Rows
+            tmpWare.Rows.Add({r.Item(1), r.Item(2), r.Item(3), Globals.findDepartmentById(r.Item(4)).name, Globals.findEmployeeById(r.Item(5)).name})
+        Next
+        dtWarehouses = tmpWare
+    End Sub
+
+    Public Function generatePage(wb As WebBrowser)
+        Dim content As String = "<html><body>"
+
+        content &= StartingPage.createTable(dtWarehouses, {"Nazwa", "Opis", "Indeks", "Wydział nadrzędny", "Pracownik odpowiedzialny"}, "Powiązania użytkownika <i>" & _emp.name & "</i> - magazyny", True)
+        'content &= createTable(dtMachines, {"Identyfikator", "Nazwa", "Opis", "Indeks", "Wydział nadrzędny", "Pracownik odpowiedzialny"}, True)
+
+
+        content &= "</body></html>"
+        wb.Document.Write(content)
+    End Function
+
+
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        wbEmployeeRelations.ShowPrintDialog()
+    End Sub
+End Class
