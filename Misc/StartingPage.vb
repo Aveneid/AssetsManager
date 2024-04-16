ffd54016904cb5bfe23e8566212a0950cc6869c5
Aveneid <dj.exers@gmail.com>
2024-04-16 13:56:12 +0200

    starting page changes

--- Misc/StartingPage.vb
+++ Misc/StartingPage.vb
@@ -2,21 +2,22 @@
 
 Module StartingPage
     Public Function generateStartingPage(wb As WebBrowser)
-        Dim content As String = "<html><body>"
+        Dim content As String = "<html><body><center>"
 
         Dim tbl = ConvertToDataTable(Of Warehouse)(Globals.warehouses)
 
-        content &= createTable(tbl, {"Identyfikator", "Nazwa", "Opis", "Indeks", "Wydział nadrzędny", "Pracownik odpowiedzialny"}, True)
+        content &= createTable(tbl, {"Identyfikator", "Nazwa", "Opis", "Indeks", "Wydział nadrzędny", "Pracownik odpowiedzialny"}, "<b> Wydziały i ich powiązania </b>", border:=True)
 
 
-        content &= "</body></html>"
+        content &= "</center></body></html>"
         wb.Document.Write(content)
     End Function
 
-    Function createTable(tbl As DataTable, columnNames() As String, Optional border As Boolean = False)
+    Function createTable(tbl As DataTable, columnNames() As String, Optional caption As String = "", Optional border As Boolean = False)
         Dim ret As String = ""
 
         ret = "<table" & If(border, " border='2'>", ">")
+        If caption <> "" Then ret &= " <caption>" & caption & "</caption>"
 
         For Each c In columnNames
             ret &= "<th>" & c & "</th>"
