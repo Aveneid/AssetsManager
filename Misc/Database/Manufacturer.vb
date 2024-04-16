c9a2ffa906b8c1e6c265e84fba6610be9d078d78
Aveneid <dj.exers@gmail.com>
2024-04-16 13:55:24 +0200

    Basic types changes

--- Misc/Database/Manufacturer.vb
+++ Misc/Database/Manufacturer.vb
@@ -1,7 +1,44 @@
 ﻿Public Class Manufacturer
-    Public Property id As Integer
-    Public Property name As String
-    Public Property location As String
-    Public Property phome As String
-    Public Property idx As String
+    Private Property _id As Integer
+    Private Property _name As String
+    Private Property _location As String
+    Private Property _phone As String
+    Private Property _idx As String
+
+    Public ReadOnly Property id As Integer
+        Get
+            Return _id
+        End Get
+    End Property
+    Public ReadOnly Property name As String
+        Get
+            Return _name
+        End Get
+    End Property
+    Public ReadOnly Property location As String
+        Get
+            Return _location
+        End Get
+    End Property
+    Public ReadOnly Property phone As String
+        Get
+            Return _phone
+        End Get
+    End Property
+    Public ReadOnly Property idx As String
+        Get
+            Return _idx
+        End Get
+    End Property
+
+    Sub New(name As String, location As String, phone As String, idx As String, Optional id As Integer = -1)
+        _id = id
+        _name = name
+        _location = location
+        _phone = phone
+        _idx = idx
+    End Sub
+    Public Overloads Function ToString()
+        Return _name
+    End Function
 End Class
---
+++ Misc/Database/Picture.vb
@@ -0,0 +1,36 @@
+﻿Public Class Picture
+    Private Property _id As Integer
+    Private Property _image As Image
+    Private Property _name As String
+
+    Public Property id As Integer
+        Get
+            Return _id
+        End Get
+        Set(value As Integer)
+            _id = value
+        End Set
+    End Property
+    Public Property name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+    Public Property image As Image
+        Get
+            Return _image
+        End Get
+        Set(value As Image)
+            _image = value
+        End Set
+    End Property
+
+    Sub New(name As String, image As Image, Optional id As Integer = -1)
+        _id = id
+        _name = name
+        _image = image
+    End Sub
+End Class
