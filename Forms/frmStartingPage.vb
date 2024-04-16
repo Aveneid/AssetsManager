96937574b29e6ac874b6c35f56dff73d118e4308
Aveneid <dj.exers@gmail.com>
2024-04-16 13:54:49 +0200

    Starting page changes

---
+++ Forms/frmStartingPage.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmStartingPage
+    Private Sub frmStartingPage_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        generateStartingPage(wbStartingPage)
+    End Sub
+
+    Private Sub frmStartingPage_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If e.CloseReason = CloseReason.UserClosing Then e.Cancel = True
+    End Sub
+
+End Class
